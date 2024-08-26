c02e63c8f05c66f66df545278508b9037478dcd3
Amr gad <150599719+amr641@users.noreply.github.com>
2024-08-26 18:56:29 +0300

    Update dbConfig.js

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,6 +1,6 @@
 import { connect } from "mongoose";
 
-const dbConn = connect(mongodb+srv://mvcApp:SJfOevfXnoMZS6ld@cluster0.rirt7.mongodb.net/mvc-saraha")
+const dbConn = connect("mongodb+srv://mvcApp:SJfOevfXnoMZS6ld@cluster0.rirt7.mongodb.net/mvc-saraha")
   .then(() => {
     console.log("dataBase connected successfully...");
   })
